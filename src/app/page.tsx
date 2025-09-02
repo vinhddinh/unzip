b823ca15622895653bd62ef2cfc546372863c0f9
vinhddinh <vinh@dinh.cc>
2025-09-02 22:45:00 +1000

    reduce padding around main component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import ZipUnzipper from "@/components/zip-unzipper";
 
 export default function Home() {
   return (
-    <main className="flex-1 min-h-0 overflow-hidden bg-background text-foreground p-6 sm:p-10">
+    <main className="flex-1 min-h-0 overflow-hidden bg-background text-foreground p-3">
       <div className="h-full min-h-0">
         <ZipUnzipper />
       </div>
